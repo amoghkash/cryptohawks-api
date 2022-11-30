75253b30a515ab6e0b03ab47d452b31bddaa0e01
amoghkash <94269573+amoghkash@users.noreply.github.com>
2022-11-30 15:56:35 -0500

    Missing Fields Error
    Added support for missing fields during login and signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,59 +1,80 @@
 const { getUserFromDB, addUserToDB } = require('../db/userDB');
 const {validateUser} = require('./authController')
 const { generateToken } = require('../scripts/jwt'); // JWT Verification
+const { validateSignupInput, validateLoginInput } = require('../scripts/inputValidation')
 
 const maxSensitiveCookieAge = 86400000
 
 const login = async (req, res, next) => {
-    // get user information from db
-    var dbUser = await getUserFromDB(req, res);
+    const {error} = validateLoginInput(req.body);
     var responseJSON
-    console.log(dbUser)
-    if(dbUser){
-        if(validateUser(req, dbUser)) {
-            var responseJSON = {
-                valid:true,
-                username: dbUser.username,
-                name: dbUser.name
+    if(!error) {
+        var dbUser = await getUserFromDB(req, res);
+        console.log(dbUser)
+        if(dbUser){
+            if(validateUser(req, dbUser)) {
+                responseJSON = {
+                    valid:true,
+                    username: dbUser.username,
+                    name: dbUser.name
+                }
+                res.status(200)
+                const authToken = generateToken(req.body.username.trim())
+                res.cookie("token", authToken, {httpOnly:true, maxAge: maxSensitiveCookieAge, secure: true, sameSite:'None'})
+                responseJSON.tokenSet = true
+            } else {    // If password is wrong
+                responseJSON = {
+                    valid:false, 
+                    issue: "Wrong Username or Password"
+                }
+                res.status(400)
             }
-            res.status(200)
-            const authToken = generateToken(req.body.username.trim())
-            res.cookie("token", authToken, {httpOnly:true, maxAge: maxSensitiveCookieAge, secure: true, sameSite:'None'})
-            responseJSON.tokenSet = true
-        } else {    // If password is wrong
-            var responseJSON = {
+        } else {    // If user doesn't exist
+            responseJSON = {
                 valid:false, 
-                issue: "Wrong Username or Password"
+                issue: "User Does not Exist"
             }
+            res.status(404)
         }
-    } else {    // If password is wrong
-        var responseJSON = {
-            valid:false, 
-            issue: "User Does not Exist"
+    } else {
+        responseJSON = {
+            valid:false,
+            issue: error.details[0].message
         }
-    }// If user doesn't exist
+        res.status(400)
+    }
     res.json(responseJSON).end()
 }
 
 const signup = async (req, res, next) => {
-    var success = await addUserToDB(req, res);
-    if(success) {
-        var responseJSON = {
-            valid:true,
-            username: req.body.username.trim(),
-            name: req.body.name.trim(),
-            firsttime: true
+    const {error} = validateSignupInput(req.body);
+    var responseJSON
+    if(!error) { // If input is valid
+        var success = await addUserToDB(req, res);
+        if(success) {
+            responseJSON = {
+                valid:true,
+                username: req.body.username.trim(),
+                name: req.body.name.trim(),
+                firsttime: true
+            }
+            const authToken = generateToken(req.body.username);
+            res.cookie("token", authToken, {httpOnly:true, maxAge: maxSensitiveCookieAge, secure: true, sameSite:'None'})
+            responseJSON.tokenSet = true
+            res.status(201);
+            
+        }else {
+            responseJSON = {
+                valid:false, 
+                issue: "User Already Exists...Please Login"
+            }
         }
-        const authToken = generateToken(req.body.username);
-        res.cookie("token", authToken, {httpOnly:true, maxAge: maxSensitiveCookieAge, secure: true, sameSite:'None'})
-        responseJSON.tokenSet = true
-        res.status(201);
-        
-    }else if(!success) {
-        var responseJSON = {
-            valid:false, 
-            issue: "User Already Exists...Please Login"
+    } else {
+        responseJSON = {
+            valid:false,
+            issue: error.details[0].message
         }
+        res.status(400)
     } 
     res.json(responseJSON).end()
 }
---
+++ src/scripts/inputValidation.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+function validateSignupInput(user) {
+    const schema = Joi.object({
+        name: Joi.string().max(50).required(),
+        username: Joi.string().max(255).required(),
+        password: Joi.string().max(255).required()
+        //subteam: Joi.string().max(255).required()
+    });
+    return schema.validate(user);
+}
+
+function validateLoginInput(user) {
+    const schema = Joi.object({
+        username: Joi.string().max(255).required(),
+        password: Joi.string().required()
+    });
+    return schema.validate(user);
+}
+
+module.exports = { validateSignupInput, validateLoginInput };
