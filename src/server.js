8f905f6fe86cdfa7814788824798fa60132273bb
amoghkash <94269573+amoghkash@users.noreply.github.com>
2022-11-30 02:31:03 -0500

    Update

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ database.once('connected', () => {
 // Setup Application
 const app = express();
 app.use(cors({
+    origin:'https://amoghkash.github.io/*',
     credentials:true
 })) // MUST BE BEFORE express.json()
 app.use(express.json())
