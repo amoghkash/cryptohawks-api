8c81883bd4ab64b7a9a392b526192eb0ce7a8642
Amogh Kashyap <94269573+amoghkash@users.noreply.github.com>
2023-01-10 15:30:12 -0500

    Allow non alphanumeric passwords

--- src/scripts/inputValidation.js
+++ src/scripts/inputValidation.js
@@ -13,8 +13,8 @@ const Joi = require('joi');
 function validateSignupInput(user) {
     const schema = Joi.object({
         name: Joi.string().max(50).required(),
-        username: Joi.string().max(255).alphanum().required(),
-        password: Joi.string().max(255).alphanum().required(),
+        username: Joi.string().max(255).required().trim(),
+        password: Joi.string().max(255).required(),
         subteam: Joi.required(),
         grade: Joi.required(),
         admin: Joi.required()
